eaad63aebf26b7978d24c17a78f965cbffe3574a
Ammy Kalsi <amrinder.kalsi13.com>
2023-09-13 17:23:21 -0400

    Week-2 Added data to IssueTable Component

--- src/components/IssueTable.jsx
+++ src/components/IssueTable.jsx
@@ -1,24 +1,34 @@
 import { Component } from 'react';
+import IssueRow from './IssueRow';
 
 class IssueTable extends Component {
     render() {
     const { issues } = this.props;
     console.log(issues); 
-    
+
+    const issuesRow = issues.map(issue => {
+        return(
+            <IssueRow issue={issue} key={issue.id}/>
+        );
+    });
 
         return(
             <div>
                 <table className="bordered-table">
                     <thead>
-                        <th>ID</th>
-                        <th>Status</th>
-                        <th>Owner</th>
-                        <th>Created</th>
-                        <th>Effort</th>
-                        <th>Completion Date</th>
-                        <th>Title</th>
+                        <tr>
+                            <th>ID</th>
+                            <th>Status</th>
+                            <th>Owner</th>
+                            <th>Created</th>
+                            <th>Effort</th>
+                            <th>Completion Date</th>
+                            <th>Title</th>
+                        </tr>
                     </thead>
-                    <tbody></tbody>
+                    <tbody>
+                        {issuesRow}
+                    </tbody>
                 </table>
             </div>
         );
